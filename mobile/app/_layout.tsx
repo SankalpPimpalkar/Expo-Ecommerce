1b543d1baa26cc052899c69a02a6307bd1830c1c
SankalpPimpalkar <pimpalkarsankalp8@gmail.com>
2025-12-14 18:40:14 +0530

    Mobile App Initialized

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,5 +1,21 @@
 import { Stack } from "expo-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { ClerkProvider } from '@clerk/clerk-expo'
+import { tokenCache } from '@clerk/clerk-expo/token-cache'
+import "../global.css"
+
+const queryClient = new QueryClient()
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ClerkProvider tokenCache={tokenCache}>
+        <Stack
+          screenOptions={{
+            headerShown: false
+          }}
+        />
+      </ClerkProvider>
+    </QueryClientProvider>
+  )
 }
